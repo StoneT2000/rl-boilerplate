f18993e6491a1bd6cd388c4091c6eda70d8dae55
Stone Tao <stonezt2019@gmail.com>
2025-04-01 12:21:21 -0700

    test

--- scripts/ppo/examples.sh
+++ scripts/ppo/examples.sh
@@ -4,9 +4,14 @@ python scripts/ppo/train.py ms3-state --env.env-id PickCube-v1 \
   --logger.no-wandb --logger.clear-out \
   --cudagraphs \
   --logger.exp-name="ppo-PickCube-v1-state-${seed}" 
-
-python scripts/ppo/train.py ms3-rgb --env.env-id PickCube-v1 \
-  --seed ${seed} \
-  --logger.no-wandb --logger.clear-out \
-  --cudagraphs \
-  --logger.exp-name="ppo-PickCube-v1-rgb-${seed}" 
+seeds=(1 2 3 4 5)
+for seed in ${seeds[@]}
+do
+  python scripts/ppo/train.py ms3-rgb --env.env-id PickCube-v1 \
+    --seed ${seed} \
+    --logger.clear-out \
+    --cudagraphs \
+    --logger.exp-name="ppo-PickCube-v1-rgb-${seed}" \
+    --logger.wandb_entity stonet2000 --logger.wandb \
+    --logger.wandb_group "PPO"
+done
